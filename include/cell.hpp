dd37531894e3c515184d98d3eeaed5385a6b9488
omargaliot <omargaliot@airspan.com>
2023-05-07 11:45:07 +0300

    code/test evaluateNewStatus
    Signed-off-by: omargaliot <omargaliot@airspan.com>

---
+++ include/cell.hpp
@@ -0,0 +1,21 @@
+#include <list>
+
+typedef enum {LIVE, DEAD, ERR} CELL_STATUS;
+typedef enum {GOOD, BAD} RC;
+
+class Cell {
+	private:
+		std::list<Cell*> neighbors;
+		CELL_STATUS status;
+		CELL_STATUS nextStatus;
+		
+	public:
+		Cell(CELL_STATUS status);
+		RC addNeighbor(Cell* neighbor);
+		CELL_STATUS EvaluateNewStatus();
+		RC UpdateNewStatus();
+		RC Print();
+		std::list<Cell*> getNeighbors();
+		CELL_STATUS getStatus();
+		CELL_STATUS getNextStatus();
+};
