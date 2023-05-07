7a161db03c20110bb14640d88d02599845bdb80e
omargaliot <omargaliot@airspan.com>
2023-05-07 10:45:52 +0300

    improve
    Signed-off-by: omargaliot <omargaliot@airspan.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
-#include <gtest/gtest.h>
-
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
 	printf("hello\n");
 	return 0;
 }
-*/
-
-TEST(SquareRootTest, NegativeNos) {
-    ASSERT_EQ(-1.0, -1);
-    ASSERT_EQ(-1.0, 2);
-}
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
